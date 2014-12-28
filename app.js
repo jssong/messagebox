0a5fecbce4b68e11f110a70f9e213194247c628d
jssong <jssong>
2014-12-28 22:01:31 +0800

    修改内容

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var leaveMsg = require('./routes/leaveMsg');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, './views/pages'));
 app.set('view engine', 'jade');
 
 app.use(favicon());
@@ -20,9 +20,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/leaveMsg', leaveMsg);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
