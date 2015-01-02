48af23b68e5027c326e2ef8761623fb5d5ff3919
jssong <jssong>
2015-01-02 12:49:51 +0800

    搭建留言页面

--- routes/leaveMsg.js
+++ routes/leaveMsg.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('leaveMsg', function(req, res) {
-  res.send('respond with a resource');
+router.get('/', function(req, res) {
+	res.render('message', { title: 'Messagebox' });
+});
+
+/* GET users listing. */
+router.post('/submit', function(req, res) {
+	res.send("Successed!");
 });
 
 module.exports = router;
